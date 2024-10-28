017544c63fa6d1c30545429bced651ebea67ba35
zhuyicheju <1279275025@qq.com>
2024-10-28 20:59:44 +0800

    rwc rdiff week2

--- week2/rwc/src/main.rs
+++ week2/rwc/src/main.rs
@@ -1,6 +1,28 @@
 use std::env;
 use std::process;
+use std::fs::File; // For read_file_lines()
+use std::io::{self, BufRead};
+fn read_file_lines(filename: &String) -> Result<Vec<String>, io::Error> {
+    let file = File::open(filename)?;
+    let mut v: Vec<String> = Vec::new();
+    for line in io::BufReader::new(file).lines() {
+        let line_str = line?;
+        v.push(line_str);
+    }
+    Ok(v)
+}
 
+fn handle(str: &Vec<String>) -> (usize, usize, usize) {
+    let length: usize = str.len();
+    let mut words : usize = 0;
+    let mut chars : usize = 0;
+
+    for val in str {
+        chars += val.len();
+        words += val.split_whitespace().count();
+    }  
+    (length, words, chars)
+}
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
@@ -8,5 +30,7 @@ fn main() {
         process::exit(1);
     }
     let filename = &args[1];
-    // Your code here :)
+    let str = read_file_lines(filename).unwrap();
+    let (lines,words,chars) = handle(&str);
+    println!("{} {} {}", lines, words, chars);
 }
